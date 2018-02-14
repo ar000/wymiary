2bcf6de80fad7b913f60f82fe4b5e5682f5e4289
AdamR <adamruc@gmail.com>
2018-02-14 07:14:45 +0100

    cztery

--- src/Wymiary.java
+++ src/Wymiary.java
@@ -11,7 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+//aaaaaa======================
 public class Wymiary{
 	private JFrame okno  = new JFrame();
 	JTextField widD = new JTextField();
